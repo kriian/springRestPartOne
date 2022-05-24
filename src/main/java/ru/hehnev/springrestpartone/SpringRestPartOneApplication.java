455e0dcb6ded23a1454c6ebd2ae9fdbeabc3be24
maniana <manianahek@gmail.com>
2022-05-24 14:14:16 +0300

    Initial commit

---
+++ src/main/java/ru/hehnev/springrestpartone/SpringRestPartOneApplication.java
@@ -0,0 +1,13 @@
+package ru.hehnev.springrestpartone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestPartOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestPartOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hehnev/springrestpartone/SpringRestPartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hehnev.springrestpartone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestPartOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
